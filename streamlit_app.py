0da99eb29830e1552b9aef51ec1b6ae728641b5d
veronika-27 <64424805+veronika-27@users.noreply.github.com>
2022-05-27 15:49:29 +0300

    Update streamlit_app.py
    typo correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ fruityvice_norm = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_norm)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.currsor()
+my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake: ")
